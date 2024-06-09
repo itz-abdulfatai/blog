0ffb9a36a5704d7091cef73cc43ad68eac1a1396
itz-abdulfatai <134066346+itz-abdulfatai@users.noreply.github.com>
2024-06-09 23:08:25 +0100

    run media query for header and hero

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,39 @@
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../images/home-images/logo.png";
 import "../styles/header.css";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
-    const navigate = useNavigate();
+  const [expanded, setExpanded] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {setExpanded(false)}, [location])
+
+  window.addEventListener("scroll", () => {
+    setExpanded(false);
+  });
+
+  // document.body.addEventListener("click", (e) => {
+  //   if (e.target !== nav && expanded) {
+  //     setExpanded(false)
+
+  //   }
+  // });
+
+  function handleMenuBar(e) {
+    !expanded && setExpanded(true);
+    expanded && setExpanded(false);
+  }
+
+  const navigate = useNavigate();
   const navigateById = (event) => {
     event.preventDefault();
     const getStartedElement = document.querySelector("#get-started");
     if (getStartedElement) {
-        getStartedElement.scrollIntoView({behavior:'smooth'})
-
+      getStartedElement.scrollIntoView({ behavior: "smooth" });
     } else {
-        navigate('*')
+      navigate("*");
     }
   };
 
@@ -21,11 +43,16 @@ const Header = () => {
         <img src={logo} alt="" className="logo" />
       </Link>
 
-      <nav>
-        <Link to="#get-started" onClick={navigateById}>get started</Link>
+      <nav className={expanded ? "nav-expanded" : undefined}>
+        <Link to="#get-started" onClick={navigateById}>
+          get started
+        </Link>
         <Link to="/blogs">Blog</Link>
         <Link to="/create">create</Link>
       </nav>
+      <div className="menu-btn" onClick={handleMenuBar}>
+        menu
+      </div>
     </div>
   );
 };
